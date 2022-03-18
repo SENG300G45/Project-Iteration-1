028bf6d70e6e3d0a300199cbff3d2b35e2fd3e1b
Harsh Patil <harshnilkanth2015@gmail.com>
2022-03-18 14:23:13 -0600

    Update StationInteractor.java

--- SCS Software/src/software/StationInteractor.java
+++ SCS Software/src/software/StationInteractor.java
@@ -8,22 +8,22 @@ import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 public class StationInteractor {
 	private static final int MAX_OBJECTS = 50;
 	private SelfCheckoutStation scs;
-	private PurchasableItem[] scannedObjects = new PurchasableItem[MAX_OBJECTS];
+	private PurchasableItem[] scannedItems = new PurchasableItem[MAX_OBJECTS];
 	private BigDecimal totalBill = BigDecimal.valueOf(0);
-	private int numberOfItems;
+	private int numberOfScannedItems;
 	
 	public StationInteractor(int maxWeight, int sensitivity) {
 		int noteDenomination[] = {5, 10, 20, 50, 100};
 		BigDecimal coinDenomination[] = {new BigDecimal(0.05), new BigDecimal(0.10), new BigDecimal(0.25), new BigDecimal(1), new BigDecimal(2)};
 		
 		scs = new SelfCheckoutStation(Currency.getInstance("CAD"), noteDenomination, coinDenomination, maxWeight, sensitivity);
-		numberOfItems = 0;
+		numberOfScannedItems = 0;
 	}
 	
 	public void scanItem(PurchasableItem purchasableItem) {
 		scs.scanner.scan(purchasableItem.item);
-		scannedObjects[numberOfItems] = purchasableItem;
-		numberOfItems++;
+		scannedItems[numberOfScannedItems] = purchasableItem;
+		numberOfScannedItems++;		
 	}
 
 }
