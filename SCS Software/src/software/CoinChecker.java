b39fc1892c2072f6ec0f813f5bbfb27189c23600
Rajarshi Sinha <rathisinha.rs@gmail.com>
2022-03-18 19:12:30 -0600

    Implemented Paying with coins

---
+++ SCS Software/src/software/CoinChecker.java
@@ -0,0 +1,46 @@
+package software;
+
+import java.math.BigDecimal;
+
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.CoinValidator;
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.CoinValidatorObserver;
+
+public class CoinChecker implements CoinValidatorObserver{
+	private BigDecimal value;
+	private boolean isValid;
+
+	@Override
+	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validCoinDetected(CoinValidator validator, BigDecimal value) {
+		this.value = value;
+		this.isValid = true;
+	}
+
+	@Override
+	public void invalidCoinDetected(CoinValidator validator) {
+		this.isValid = false;
+		
+	}
+	
+	public BigDecimal getValue() {
+		return value;
+	}
+	
+	public boolean checkValid() {
+		return isValid;
+	}
+
+}
--- SCS Software/src/software/StationInteractor.java
+++ SCS Software/src/software/StationInteractor.java
@@ -3,14 +3,21 @@ package software;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import org.lsmr.selfcheckout.Coin;
+import org.lsmr.selfcheckout.devices.DisabledException;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.observers.CoinValidatorObserver;
 
 public class StationInteractor {
 	private static final int MAX_OBJECTS = 50;
 	private SelfCheckoutStation scs;
 	private PurchasableItem[] scannedItems = new PurchasableItem[MAX_OBJECTS];
 	private BigDecimal totalBill = BigDecimal.valueOf(0);
+<<<<<<< Updated upstream
 	private int numberOfScannedItems;
+=======
+	private BigDecimal paidAmountWithCoins = BigDecimal.ZERO;
+>>>>>>> Stashed changes
 	
 	public StationInteractor(int maxWeight, int sensitivity) {
 		int noteDenomination[] = {5, 10, 20, 50, 100};
@@ -20,10 +27,44 @@ public class StationInteractor {
 		numberOfScannedItems = 0;
 	}
 	
+<<<<<<< Updated upstream
 	public void scanItem(PurchasableItem purchasableItem) {
 		scs.scanner.scan(purchasableItem.item);
 		scannedItems[numberOfScannedItems] = purchasableItem;
 		numberOfScannedItems++;
 	}
+=======
+	public void checkout(boolean isPayingWithCoins) {
+		if(isPayingWithCoins) {
+			
+			while(paidAmountWithCoins.compareTo(totalBill) < 0) {
+				Coin dollar = new Coin(scs.coinDenominations.get(3));
+
+				try {
+					addCoin(dollar);
+				} catch (DisabledException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		else {
+			// Pay with banknotes
+		}
+		
+	}
+	
+	private void addCoin(Coin coin) throws DisabledException {
+		
+		CoinChecker checker = new CoinChecker();
+		scs.coinValidator.attach(checker);
+		scs.coinValidator.accept(coin);
+		if(checker.checkValid()) {
+			paidAmountWithCoins.add(checker.getValue());
+		}
+		
+		
+	}
+	
+>>>>>>> Stashed changes
 
 }
