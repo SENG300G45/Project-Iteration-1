ef0329c8de1623d0d1cc897b78002dd6b9e15f35
Arthur <36118607+Arthurpod@users.noreply.github.com>
2022-03-19 17:29:56 -0600

    Add files via upload

---
+++ SCS Software/src/testing/placeItemTest.java
@@ -0,0 +1,10 @@
+package testing;
+
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.Numeral;
+
+public class placeItemTest {
+	Numeral numeral[] = {Numeral.one, Numeral.two};
+	Barcode b = new Barcode(numeral);
+	
+}
