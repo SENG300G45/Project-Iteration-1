ea224d7a48d35f6b87a3428636c9c6b42ff723c1
Harsh Patil <harshnilkanth2015@gmail.com>
2022-03-20 15:11:24 -0600

    Revert "Merge branch 'scanning-testing' of https://github.com/SENG300G45/Project-Iteration-1 into scanning-testing"
    This reverts commit 03db7ccbdc6a67f4afa06ea9ea072f6c401a74d7, reversing
    changes made to 3787e102a7e2bee4ab1765ebe1d8ceaa973c1185.

--- SCS Software/src/software/Main.java
+++ SCS Software/src/software/Main.java
@@ -1,11 +1,13 @@
 package software;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 import java.util.Random;
 
 import org.lsmr.selfcheckout.Barcode;
 import org.lsmr.selfcheckout.BarcodedItem;
 import org.lsmr.selfcheckout.Numeral;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 
 public class Main {
 	private static final int MAX_OBJECTS = 50;
@@ -16,7 +18,6 @@ public class Main {
 		PurchasableItem sampleItem = GenerateItem(100, new BigDecimal(20), "ABC Soap");
 		
 		station.scanItem(sampleItem);
-		System.out.println(sampleItem.item.getBarcode().toString());
 		
 	}
 	
@@ -29,6 +30,7 @@ public class Main {
 		 */
 		final int MAX_CODES = 12;
 		
+		PurchasableItem item;
 		Numeral code[] = new Numeral[MAX_CODES];
 		Random rand = new Random();
 		
@@ -37,7 +39,7 @@ public class Main {
 		}
 		
 		Barcode barcode = new Barcode(code);
-		return new PurchasableItem(new BarcodedItem(barcode,weight), price, desc);
+		return item = new PurchasableItem(new BarcodedItem(barcode,weight), price, desc);
 	}
 
 }
--- SCS Software/src/software/StationInteractor.java
+++ SCS Software/src/software/StationInteractor.java
@@ -1,84 +1,29 @@
 package software;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Currency;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.lsmr.selfcheckout.*;
-import org.lsmr.selfcheckout.BarcodedItem;
-import org.lsmr.selfcheckout.Numeral;
-import org.lsmr.selfcheckout.devices.AbstractDevice;
-import org.lsmr.selfcheckout.devices.BarcodeScanner;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
-import org.lsmr.selfcheckout.devices.SimulationException;
-import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
-import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
 
-import org.lsmr.selfcheckout.products.*;
-
-import observers.ScanCheckerObserver;
-
-public class StationInteractor implements BarcodeScannerObserver {
+public class StationInteractor {
 	private static final int MAX_OBJECTS = 50;
-	private SelfCheckoutStation selfCheckoutStation;
+	private SelfCheckoutStation scs;
 	private PurchasableItem[] scannedItems = new PurchasableItem[MAX_OBJECTS];
 	private BigDecimal totalBill = BigDecimal.valueOf(0);
 	private int numberOfScannedItems;
-	public Barcode itemBarcode;
-	public double itemWeight;
-	//public List<PurchasableItem> itemCatalog = new ArrayList<PurchasableItem>();
-	public Map<Barcode, BarcodedItem> map = new HashMap<Barcode, BarcodedItem>();
-	public BarcodedItem matchingBarcodedItem = null;
-
 	
-	
-	public StationInteractor(SelfCheckoutStation scs) {
-		selfCheckoutStation = scs;
-		scs.scanner.attach(this);
+	public StationInteractor(int maxWeight, int sensitivity) {
+		int noteDenomination[] = {5, 10, 20, 50, 100};
+		BigDecimal coinDenomination[] = {new BigDecimal(0.05), new BigDecimal(0.10), new BigDecimal(0.25), new BigDecimal(1), new BigDecimal(2)};
 		
-		//List of Purchasableitems in catalog
-		Numeral numeral[] = {Numeral.one, Numeral.two};
-		Barcode b = new Barcode(numeral);
-		BarcodedItem redAppleBarcodedItem = new BarcodedItem(b, 10.0);
-		BigDecimal redApplePrice = new BigDecimal("1.50");
-		//PurchasableItem redApple = new PurchasableItem(redAppleBarcodedItem, redApplePrice, "red apple");
-		
-		//itemCatalog.add(redApple);
-		map.put(b, redAppleBarcodedItem);
+		scs = new SelfCheckoutStation(Currency.getInstance("CAD"), noteDenomination, coinDenomination, maxWeight, sensitivity);
+		numberOfScannedItems = 0;
 	}
 	
-	
-
 	public void scanItem(PurchasableItem purchasableItem) {
-		selfCheckoutStation.scanner.scan(purchasableItem.item);
-		matchingBarcodedItem = map.get(itemBarcode);
-		
-		if (matchingBarcodedItem != null) {
-			itemWeight = matchingBarcodedItem.getWeight();
-			scannedItems[numberOfScannedItems] = purchasableItem;
-			numberOfScannedItems++;
-		}
-		
-		
-			
-
-	}
-	
-	@Override
-	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		
-	}
-	@Override
-	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		
-	}
-	@Override
-	public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
-		itemBarcode = barcode;
-		
+		scs.scanner.scan(purchasableItem.item);
+		scannedItems[numberOfScannedItems] = purchasableItem;
+		numberOfScannedItems++;
 	}
 
 }
