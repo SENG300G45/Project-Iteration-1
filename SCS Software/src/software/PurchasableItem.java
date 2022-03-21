223f3a09ef93ea979526a82b8681fb0b8355cd72
Arthur <36118607+Arthurpod@users.noreply.github.com>
2022-03-20 22:17:38 -0600

    Add files via upload

--- SCS Software/src/software/PurchasableItem.java
+++ SCS Software/src/software/PurchasableItem.java
@@ -10,24 +10,33 @@ public class PurchasableItem {
 	public BarcodedProduct product;
 	public BarcodedItem item;
 	
+	/**PurchasableItem is an object that associates an item to a price and a description
+	 * @param item
+	 * @param price
+	 * @param description
+	 */
 	public PurchasableItem(BarcodedItem item, BigDecimal price, String description) {
 		this.item = item;
 		this.code = item.getBarcode();
 		this.product = new BarcodedProduct(code, description, price);		
 	}
 	
+	//get barcode
 	public Barcode getCode() {
 		return this.code;
 	}
 	
+	//get price of associated barcodedItem
 	public BigDecimal getPrice() {
 		return this.product.getPrice();
 	}
 	
+	//get weight associated barcodedItem
 	public double getWeight() {
 		return this.item.getWeight();
 	}
 	
+	//get description of associated barcodedItem
 	public String getDescription() {
 		return this.product.getDescription();
 	}
